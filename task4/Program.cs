8c09e37d0bffb1a0b041220e75a09e645f74feaa
Viacheslav Burkov <vburkov77mob@gmail.com>
2023-02-24 13:38:37 +0300

    task4 added and solved

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+double a;
+double b;
+double c;
+double max;
+
+a = 4;
+b = 2;
+c = 3;
+max = a;
+
+if (b>max) max = b;
+if (c>max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
